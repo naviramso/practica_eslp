644f51730d96798825eb6d3533489cbab9f113c0
Ivan Palacios <ivpalacios47@gmail.com>
2023-05-31 13:12:10 -0400

    base de conocimiento IvanP

--- Prolog Practica/base_conocimiento.pl
+++ Prolog Practica/base_conocimiento.pl
@@ -1,35 +1,104 @@
-genero(comedia).
-genero(accion).
-genero(terror).
-genero(drama).
-genero(animacion).
-genero(suspenso).
-genero(fantasia).
+% Películas y sus características
+pelicula('Taxi Driver', drama, 'La historia de Travis Bickle, un veterano de Vietnam que trabaja como taxista y se sumerge en la decadencia de Nueva York.').
+actores('Taxi Driver', ['Robert De Niro', 'Jodie Foster']).
 
-actor('Robert De Niro').
-actor('Johnny Deep').
-actor('Jim Carrey').
-actor('Al Pachino').
+pelicula('Raging Bull', drama, 'La vida del boxeador Jake LaMotta y su ascenso y caída en el mundo del boxeo, marcada por la autodestrucción y la violencia.').
+actores('Raging Bull', ['Robert De Niro', 'Joe Pesci']).
 
-actor_pelicula('Robert De Niro', 'Taxi Driver').
-actor_pelicula('Al Pachino', 'El padrino 2').
-actor_pelicula('Robert De Niro', 'El padrino 2').
-actor_pelicula('Johnny Deep', 'Joven Manos de Tijeras').
-actor_pelicula('Johnny Deep', 'El barbero demoniaco').
-actor_pelicula('Johnny Deep', 'Rango').
-actor_pelicula('Jim Carrey', 'Eterno resplandor de una mente sin recuerdo').
-actor_pelicula('Al Pachino', 'Jack y Jill').
-actor_pelicula('Johnny Deep', 'El jinete sin cabeza').
-actor_pelicula('Robert De Niro', 'Toro Salvaje').
+pelicula('Goodfellas', drama, 'La historia de la vida real del gánster Henry Hill y su participación en la mafia italiana en Nueva York.').
+actores('Goodfellas', ['Robert De Niro', 'Ray Liotta', 'Joe Pesci']).
 
-pelicula_genero('Taxi Driver' , drama).
-pelicula_genero('El padrino 2', drama).
-pelicula_genero('Joven Manos de Tijeras', fantasia).
-pelicula_genero('El barbero demoniaco', suspenso).
-pelicula_genero('Rango', animacion).
-pelicula_genero('Eterno resplando de una mente sin recuerdo', fantasia).
-pelicula_genero('Jack y Jill', comedia).
-pelicula_genero('El jinete sin cabeza', terror).
-pelicula_genero('Toro Salvaje', accion).
+pelicula('The Godfather Part II', drama, 'Una secuela que sigue la vida de Vito Corleone y su hijo Michael, explorando su ascenso al poder en el mundo del crimen organizado.').
+actores('The Godfather Part II', ['Robert De Niro', 'Al Pacino']).
 
-actor_genero(X,Y,Z):- actor_pelicula(X,Y) , pelicula_genero(Y,Z).
+pelicula('Heat', accion, 'El enfrentamiento entre un ladrón de bancos y un tenaz detective de la policía de Los Ángeles, mientras ambos hombres persiguen sus propios códigos de honor.').
+actores('Heat', ['Robert De Niro', 'Al Pacino']).
+
+pelicula('The Irishman', drama, 'La historia de Frank Sheeran, un asesino a sueldo involucrado con la mafia y su supuesta participación en la desaparición del líder sindical Jimmy Hoffa.').
+actores('The Irishman', ['Robert De Niro', 'Al Pacino', 'Joe Pesci']).
+
+pelicula('Scarface', drama, 'La historia de Tony Montana, un inmigrante cubano que se convierte en un poderoso narcotraficante en Miami.').
+actores('Scarface', ['Al Pacino', 'Michelle Pfeiffer']).
+
+pelicula('Serpico', drama, 'Basada en la vida real del policía Frank Serpico, que expone la corrupción policial en la ciudad de Nueva York.').
+actores('Serpico', ['Al Pacino']).
+
+pelicula('Dog Day Afternoon', drama, 'La historia de un atraco bancario fallido y el caos que sigue, basada en un evento real ocurrido en la década de 1970.').
+actores('Dog Day Afternoon', ['Al Pacino']).
+
+pelicula('The Godfather Part III', drama, 'La tercera parte de la saga El Padrino, que sigue la vida de Michael Corleone mientras intenta proteger a su familia y su imperio.').
+actores('The Godfather Part III', ['Al Pacino', 'Andy Garcia']).
+
+pelicula('The Devils Advocate', drama, 'La historia de un joven abogado talentoso que se une a un poderoso bufete de abogados liderado por un hombre carismático y diabólico.').
+actores('The Devils Advocate', ['Keanu Reeves', 'Al Pacino', 'Charlize Theron']).
+
+pelicula('Pirates of the Caribbean: The Curse of the Black Pearl', aventura, 'El capitán Jack Sparrow se embarca en una aventura en alta mar para rescatar a una doncella secuestrada y recuperar un tesoro legendario.').
+actores('Pirates of the Caribbean: The Curse of the Black Pearl', ['Johnny Depp', 'Orlando Bloom', 'Keira Knightley']).
+
+pelicula('Edward Scissorhands', fantasia, 'La historia de Edward, un hombre con manos de tijera que es llevado a vivir en un suburbio y se enamora de una joven.').
+actores('Edward Scissorhands', ['Johnny Depp', 'Winona Ryder']).
+
+pelicula('Charlie and the Chocolate Factory', fantasia, 'Basada en el libro de Roald Dahl, sigue las aventuras de Charlie Bucket en la fábrica de chocolate del excéntrico Willy Wonka.').
+actores('Charlie and the Chocolate Factory', ['Johnny Depp']).
+
+pelicula('Sweeney Todd: The Demon Barber of Fleet Street', musical, 'La historia de un barbero que busca venganza y se convierte en asesino mientras se une a una pastelera para deshacerse de los cuerpos.').
+actores('Sweeney Todd: The Demon Barber of Fleet Street', ['Johnny Depp', 'Helena Bonham Carter']).
+
+pelicula('Alice in Wonderland', aventura, 'Una adaptación del clásico cuento de Lewis Carroll, donde una joven Alice se sumerge en un mundo mágico y surrealista.').
+actores('Alice in Wonderland', ['Johnny Depp', 'Mia Wasikowska']).
+
+pelicula('Finding Neverland', drama, 'La historia de J.M. Barrie, el autor de Peter Pan, y su inspiración para escribir la famosa obra teatral.').
+actores('Finding Neverland', ['Johnny Depp', 'Kate Winslet']).
+
+pelicula('Black Mass', crimen, 'Basada en la vida real de Whitey Bulger, un infame gánster de Boston que colabora con el FBI mientras controla el crimen organizado de la ciudad.').
+actores('Black Mass', ['Johnny Depp', 'Benedict Cumberbatch']).
+
+pelicula('Sleepy Hollow', horror, 'Una adaptación del cuento de Washington Irving, donde un detective investiga una serie de asesinatos en un pueblo embrujado.').
+actores('Sleepy Hollow', ['Johnny Depp', 'Christina Ricci']).
+
+pelicula('Iron Man', accion, 'La historia del millonario Tony Stark, quien se convierte en el superhéroe Iron Man para luchar contra el mal y proteger al mundo.').
+actores('Iron Man', ['Robert Downey Jr.', 'Gwyneth Paltrow']).
+
+pelicula('Sherlock Holmes', accion, 'Basada en el famoso detective creado por Arthur Conan Doyle, sigue las aventuras de Sherlock Holmes y su compañero Watson mientras resuelven misterios.').
+actores('Sherlock Holmes', ['Robert Downey Jr.', 'Jude Law']).
+
+pelicula('The Avengers', accion, 'Un grupo de superhéroes se unen para proteger la Tierra de una amenaza alienígena, liderados por Iron Man, interpretado por Robert Downey Jr.').
+actores('The Avengers', ['Robert Downey Jr.', 'Chris Evans', 'Chris Hemsworth']).
+
+pelicula('Tropic Thunder', comedia, 'Un grupo de actores de Hollywood se encuentra en medio de un conflicto armado real mientras filman una película de guerra.').
+actores('Tropic Thunder', ['Robert Downey Jr.', 'Ben Stiller', 'Jack Black']).
+
+pelicula('La La Land', musical, 'Un pianista de jazz y una aspirante a actriz se encuentran y se enamoran en Los Ángeles mientras persiguen sus sueños en el mundo del espectáculo.').
+actores('La La Land', ['Emma Stone', 'Ryan Gosling']).
+
+pelicula('Easy A', comedia, 'Una estudiante de secundaria inventa un rumor sobre su vida sexual para mejorar su reputación, pero las cosas se salen de control.').
+actores('Easy A', ['Emma Stone', 'Penn Badgley']).
+
+pelicula('The Help', drama, 'La historia de varias mujeres afroamericanas que trabajan como empleadas domésticas en los años 60 en el sur de Estados Unidos y su lucha por la igualdad.').
+actores('The Help', ['Emma Stone', 'Viola Davis']).
+
+pelicula('Birdman or (The Unexpected Virtue of Ignorance)', comedia_drama, 'Un actor de renombre intenta revitalizar su carrera en Broadway mientras lidia con problemas personales y la voz en su cabeza.').
+actores('Birdman or (The Unexpected Virtue of Ignorance)', ['Emma Stone', 'Michael Keaton']).
+
+pelicula('Men in Black', ciencia_ficcion, 'Un agente secreto se une a una organización encargada de proteger a la Tierra de extraterrestres, enfrentando situaciones cómicas y peligrosas.').
+actores('Men in Black', ['Will Smith', 'Tommy Lee Jones']).
+
+pelicula('Independence Day', ciencia_ficcion, 'En el día de la independencia de Estados Unidos, la humanidad se enfrenta a una invasión alienígena y un piloto de combate lucha por salvar al mundo.').
+actores('Independence Day', ['Will Smith', 'Jeff Goldblum']).
+
+pelicula('I Am Legend', ciencia_ficcion, 'El último hombre en la Tierra lucha por sobrevivir en un mundo postapocalíptico lleno de criaturas mutantes y busca una cura para la enfermedad que los ha convertido.').
+actores('I Am Legend', ['Will Smith']).
+
+pelicula('The Pursuit of Happyness', drama, 'Basada en la historia real de Chris Gardner, un hombre que lucha por superar la adversidad y alcanzar el éxito profesional mientras cuida a su hijo.').
+actores('The Pursuit of Happyness', ['Will Smith', 'Jaden Smith']).
+
+recomendar_peliculas(Generos, ActoresFavoritos, PeliculasRecomendadas) :-
+    findall(Pelicula, (
+        pelicula(Pelicula, Genero, _),
+        member(Genero, Generos),
+        actores(Pelicula, Actores),
+        intersection(ActoresFavoritos, Actores, ActoresEnComun),
+        length(ActoresEnComun, CantidadActoresEnComun),
+        CantidadActoresEnComun > 0
+    ), PeliculasRecomendadas).
+
