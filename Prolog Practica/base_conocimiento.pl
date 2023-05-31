b24b8634950be4db63ed3c890b67b3b68e9a0e0f
Ivan Palacios <ivpalacios47@gmail.com>
2023-05-31 18:30:35 -0400

    imagenes

--- Prolog Practica/base_conocimiento.pl
+++ Prolog Practica/base_conocimiento.pl
@@ -3,55 +3,55 @@
 pelicula('Taxi Driver', drama, 'La historia de Travis Bickle, un veterano de Vietnam que trabaja como taxista y se sumerge en la decadencia de Nueva York.','taxi.jpg').
 actores('Taxi Driver', ['Robert De Niro', 'Jodie Foster']).
 
-pelicula('Raging Bull', drama, 'La vida del boxeador Jake LaMotta y su ascenso y caída en el mundo del boxeo, marcada por la autodestrucción y la violencia.').
+pelicula('Raging Bull', drama, 'La vida del boxeador Jake LaMotta y su ascenso y caída en el mundo del boxeo, marcada por la autodestrucción y la violencia.', 'ranging.jpg').
 actores('Raging Bull', ['Robert De Niro', 'Joe Pesci']).
 
-pelicula('Goodfellas', drama, 'La historia de la vida real del gánster Henry Hill y su participación en la mafia italiana en Nueva York.').
+pelicula('Goodfellas', drama, 'La historia de la vida real del gánster Henry Hill y su participación en la mafia italiana en Nueva York.', 'goodfellas.jpg').
 actores('Goodfellas', ['Robert De Niro', 'Ray Liotta', 'Joe Pesci']).
 
-pelicula('The Godfather Part II', drama, 'Una secuela que sigue la vida de Vito Corleone y su hijo Michael, explorando su ascenso al poder en el mundo del crimen organizado.').
+pelicula('The Godfather Part II', drama, 'Una secuela que sigue la vida de Vito Corleone y su hijo Michael, explorando su ascenso al poder en el mundo del crimen organizado.','elpadrino2.jpeg').
 actores('The Godfather Part II', ['Robert De Niro', 'Al Pacino']).
 
-pelicula('Heat', accion, 'El enfrentamiento entre un ladrón de bancos y un tenaz detective de la policía de Los Ángeles, mientras ambos hombres persiguen sus propios códigos de honor.').
+pelicula('Heat', accion, 'El enfrentamiento entre un ladrón de bancos y un tenaz detective de la policía de Los Ángeles, mientras ambos hombres persiguen sus propios códigos de honor.','heat.jpeg').
 actores('Heat', ['Robert De Niro', 'Al Pacino']).
 
-pelicula('The Irishman', drama, 'La historia de Frank Sheeran, un asesino a sueldo involucrado con la mafia y su supuesta participación en la desaparición del líder sindical Jimmy Hoffa.').
+pelicula('The Irishman', drama, 'La historia de Frank Sheeran, un asesino a sueldo involucrado con la mafia y su supuesta participación en la desaparición del líder sindical Jimmy Hoffa.','irishman.jpg').
 actores('The Irishman', ['Robert De Niro', 'Al Pacino', 'Joe Pesci']).
 
-pelicula('Scarface', drama, 'La historia de Tony Montana, un inmigrante cubano que se convierte en un poderoso narcotraficante en Miami.').
+pelicula('Scarface', drama, 'La historia de Tony Montana, un inmigrante cubano que se convierte en un poderoso narcotraficante en Miami.', 'scarface.jpg').
 actores('Scarface', ['Al Pacino', 'Michelle Pfeiffer']).
 
-pelicula('Serpico', drama, 'Basada en la vida real del policía Frank Serpico, que expone la corrupción policial en la ciudad de Nueva York.').
+pelicula('Serpico', drama, 'Basada en la vida real del policía Frank Serpico, que expone la corrupción policial en la ciudad de Nueva York.','serpico.jpeg').
 actores('Serpico', ['Al Pacino']).
 
-pelicula('Dog Day Afternoon', drama, 'La historia de un atraco bancario fallido y el caos que sigue, basada en un evento real ocurrido en la década de 1970.').
+pelicula('Dog Day Afternoon', drama, 'La historia de un atraco bancario fallido y el caos que sigue, basada en un evento real ocurrido en la década de 1970.', 'dogday.jpg').
 actores('Dog Day Afternoon', ['Al Pacino']).
 
-pelicula('The Godfather Part III', drama, 'La tercera parte de la saga El Padrino, que sigue la vida de Michael Corleone mientras intenta proteger a su familia y su imperio.').
+pelicula('The Godfather Part III', drama, 'La tercera parte de la saga El Padrino, que sigue la vida de Michael Corleone mientras intenta proteger a su familia y su imperio.', 'elpadrino3.jpg').
 actores('The Godfather Part III', ['Al Pacino', 'Andy Garcia']).
 
-pelicula('The Devils Advocate', drama, 'La historia de un joven abogado talentoso que se une a un poderoso bufete de abogados liderado por un hombre carismático y diabólico.').
+pelicula('The Devils Advocate', drama, 'La historia de un joven abogado talentoso que se une a un poderoso bufete de abogados liderado por un hombre carismático y diabólico.','deviladvocate.jpg').
 actores('The Devils Advocate', ['Keanu Reeves', 'Al Pacino', 'Charlize Theron']).
 
-pelicula('Pirates of the Caribbean: The Curse of the Black Pearl', aventura, 'El capitán Jack Sparrow se embarca en una aventura en alta mar para rescatar a una doncella secuestrada y recuperar un tesoro legendario.').
+pelicula('Pirates of the Caribbean: The Curse of the Black Pearl', aventura, 'El capitán Jack Sparrow se embarca en una aventura en alta mar para rescatar a una doncella secuestrada y recuperar un tesoro legendario.', 'piratas.jpg').
 actores('Pirates of the Caribbean: The Curse of the Black Pearl', ['Johnny Depp', 'Orlando Bloom', 'Keira Knightley']).
 
-pelicula('Edward Scissorhands', fantasia, 'La historia de Edward, un hombre con manos de tijera que es llevado a vivir en un suburbio y se enamora de una joven.').
+pelicula('Edward Scissorhands', fantasia, 'La historia de Edward, un hombre con manos de tijera que es llevado a vivir en un suburbio y se enamora de una joven.', 'scissor.jpg').
 actores('Edward Scissorhands', ['Johnny Depp', 'Winona Ryder']).
 
-pelicula('Charlie and the Chocolate Factory', fantasia, 'Basada en el libro de Roald Dahl, sigue las aventuras de Charlie Bucket en la fábrica de chocolate del excéntrico Willy Wonka.').
+pelicula('Charlie and the Chocolate Factory', fantasia, 'Basada en el libro de Roald Dahl, sigue las aventuras de Charlie Bucket en la fábrica de chocolate del excéntrico Willy Wonka.','charli.jpeg').
 actores('Charlie and the Chocolate Factory', ['Johnny Depp']).
 
-pelicula('Sweeney Todd: The Demon Barber of Fleet Street', musical, 'La historia de un barbero que busca venganza y se convierte en asesino mientras se une a una pastelera para deshacerse de los cuerpos.').
+pelicula('Sweeney Todd: The Demon Barber of Fleet Street', musical, 'La historia de un barbero que busca venganza y se convierte en asesino mientras se une a una pastelera para deshacerse de los cuerpos.', 'barbero.jpeg').
 actores('Sweeney Todd: The Demon Barber of Fleet Street', ['Johnny Depp', 'Helena Bonham Carter']).
 
-pelicula('Alice in Wonderland', aventura, 'Una adaptación del clásico cuento de Lewis Carroll, donde una joven Alice se sumerge en un mundo mágico y surrealista.').
+pelicula('Alice in Wonderland', aventura, 'Una adaptación del clásico cuento de Lewis Carroll, donde una joven Alice se sumerge en un mundo mágico y surrealista.', 'alice.jpg').
 actores('Alice in Wonderland', ['Johnny Depp', 'Mia Wasikowska']).
 
-pelicula('Finding Neverland', drama, 'La historia de J.M. Barrie, el autor de Peter Pan, y su inspiración para escribir la famosa obra teatral.').
+pelicula('Finding Neverland', drama, 'La historia de J.M. Barrie, el autor de Peter Pan, y su inspiración para escribir la famosa obra teatral.', 'finding.jpg').
 actores('Finding Neverland', ['Johnny Depp', 'Kate Winslet']).
 
-pelicula('Black Mass', crimen, 'Basada en la vida real de Whitey Bulger, un infame gánster de Boston que colabora con el FBI mientras controla el crimen organizado de la ciudad.').
+pelicula('Black Mass', crimen, 'Basada en la vida real de Whitey Bulger, un infame gánster de Boston que colabora con el FBI mientras controla el crimen organizado de la ciudad.', 'black.jpg').
 actores('Black Mass', ['Johnny Depp', 'Benedict Cumberbatch']).
 
 pelicula('Sleepy Hollow', horror, 'Una adaptación del cuento de Washington Irving, donde un detective investiga una serie de asesinatos en un pueblo embrujado.').
